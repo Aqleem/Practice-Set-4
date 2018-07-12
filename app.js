59809229416db152e4fd25a1767a797443e5714f
VB Student <vbstudent@uit.edu>
2018-07-12 17:38:38 +0500

    is ok

---
+++ app.js
@@ -0,0 +1,99 @@
+                      // Q1
+
+// function squareTheNumber(){
+// let num = document.querySelector("input").value
+
+//     console.log(num*num)
+
+
+// }
+
+                        // Q2
+
+// function cubeTheNumber(){
+// let num = document.querySelector("input").value
+
+//     console.log(num*num*num);}
+
+                               //Q3
+
+// function addTheNumber(){
+// let number1 =parseInt(document.querySelector("#num1").value);
+// let  number2= parseInt(document.querySelector("#num2").value);
+
+
+//     console.log(number1 + number2);}
+                                 //  Q4
+// let appleCost = 10;
+// let bananaCost = 15;
+// let mangoCost = 8;
+
+//     function theCost(){
+//         let apple = parseInt(document.querySelector("#apple").value);
+//         let banana = parseInt(document.querySelector("#banana").value);
+//         let mango = parseInt(document.querySelector("#mango").value);
+
+//         let cost  = (apple*appleCost)+(banana*bananaCost)+(mango*mangoCost);
+//         alert(cost);
+
+//     }                            
+// ...................................................................................
+                                      //Q5
+
+//   function percen(){
+//       let number=parseInt (document.querySelector("input").value)
+//       console.log(number/1050*100);
+//   }     
+
+// ..........................................................................................
+
+
+//                                    Q6
+
+
+    //    function  checkTheNum(){
+    // let englishNumber=parseInt (document.querySelector("#english").value)
+    // let mathNumber=parseInt (document.querySelector("#math").value)
+    // let computerNumber=parseInt (document.querySelector("#computer").value)
+
+    // if(englishNumber >= 10 && mathNumber >= 20 && computerNumber >= 50){
+    //      console.log("congratulations you passed")
+
+    // }
+    // else{
+    //     console.log ("sorry you Fail")
+    // }
+
+    //    }
+    // ...........................................................................................
+                                            //Q7
+    // function check(){
+    //     let userSpeed = document.querySelector("input").value;
+    //     let para = document.querySelector("p");
+    //     if(userSpeed > 60 ) {
+    //         para.innerHTML = "OVERSPEEDING"
+
+    //     } else {
+    //         para.innerHTML = "NOT OVERSPEEDING"
+    //     }
+    // }
+
+
+
+    // ..................................................................................................
+                                          //Q7
+
+//    function welcome(){
+//        let firstName = document.querySelector("input").value
+//        alert("Welcome to " + firstName )
+//    }                                        
+
+//........................................................................................................
+                                  //Q8
+
+
+  function addThis(){
+      let item = document.querySelector("input").value;
+      let pge = document.querySelector("ol");
+      pge.innerHTML+= "<li>"+item+"</li>";
+  }                                
